8e8c3d2f96d4b37acb1d056d5e6d19c6d6b21e84
Bruno Bernado Tremarin <bruno-tremarin@hotmail.com>
2023-09-25 15:00:33 -0300

    Adicionando um projeto spring

---
+++ api/tales-api/src/main/java/com/tales/talesapi/TalesApiApplication.java
@@ -0,0 +1,13 @@
+package com.tales.talesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalesApiApplication.class, args);
+	}
+
+}
