aa1d761d78fb60363e3ed20f91fc76ed797db19c
Nícolas Cardoso <nmonteirocardoso@gmail.com>
2023-11-30 23:08:34 -0300

    a

--- api/tales-api/src/main/java/com/tales/talesapi/services/TokenService.java
+++ api/tales-api/src/main/java/com/tales/talesapi/services/TokenService.java
@@ -24,7 +24,7 @@ public class TokenService {
 	private static final Logger logger = LoggerFactory.getLogger(TokenService.class);
 	private String secret = "tales";
 	private Instant dataExpiracao = LocalDateTime.now()
-			.plusMinutes(1)
+			.plusHours(5)
 			.toInstant(ZoneOffset.of("-03:00"));
 	
 	public String gerarToken(Usuario user) {
