f69d13e3888f7e200b088c7dcbf476dc7ca7d09d
Bruno Bernado Tremarin <bruno-tremarin@hotmail.com>
2023-12-06 12:22:37 -0300

    Adicionando SQL file

---
+++ banco-de-dados/TalesTags.sql
@@ -0,0 +1,12 @@
+USE tales;
+INSERT INTO tag (nome) VALUES ("4K");
+INSERT INTO tag (nome) VALUES ("4I");
+INSERT INTO tag (nome) VALUES ("3K");
+INSERT INTO tag (nome) VALUES ("3I");
+INSERT INTO tag (nome) VALUES ("2K");
+INSERT INTO tag (nome) VALUES ("2I");
+INSERT INTO tag (nome) VALUES ("1K");
+INSERT INTO tag (nome) VALUES ("1I");
+
+
+
